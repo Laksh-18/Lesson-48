6adc1c4ddd4b4959cca0bbbd21f94d55edda8df2
Laksh18 <sahasra.m10@gmail.com>
2026-01-20 06:37:00 +0100

    Lesson 48

---
+++ Cat and dog.py
@@ -0,0 +1,32 @@
+# Define a class named Cat
+class Cat:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("Meow")
+
+# Define a class named Dog
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+
+    def make_sound(self):
+        print("Bark")
+
+# Create objects
+cat1 = Cat("Dodo", 2.5)
+dog1 = Dog("Tyson", 8)
+
+# Polymorphism loop
+for animal in (cat1, dog1):
+    animal.make_sound()
+    animal.info()
